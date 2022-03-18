9ef3a3689c21742dab30f40f4c047f3a929099db
Osmin Lopez <arielopez229422@hotmail.com>
2022-03-18 09:07:49 -0600

    feching data avo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [products, setProducts] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({ data, length }) => {
+        setProducts(data)
+      })
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {products.map((product) => (
+        <div>{product.name}</div>
+      ))}
     </div>
   )
 }
